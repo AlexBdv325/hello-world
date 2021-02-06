aa5d6665a83a6a235945207d39e2bcc71d84be64
AlexBdv325 <78662277+AlexBdv325@users.noreply.github.com>
2021-02-06 23:30:56 +0300

    Update PROC_EMP_CURSOR.sql

--- PROC_EMP_CURSOR.sql
+++ PROC_EMP_CURSOR.sql
@@ -4,15 +4,15 @@ PROCEDURE proc_emp_cursor is
   CURSOR c_emp_cursor IS 
    SELECT employee_id, last_name FROM employees
    WHERE department_id =30;
-  v_empno employees.employee_id%TYPE;
-  v_lname employees.last_name%TYPE;
+  l_empno employees.employee_id%TYPE;
+  l_lname employees.last_name%TYPE;
 BEGIN  OPEN c_emp_cursor;
   LOOP
-    FETCH c_emp_cursor INTO v_empno, v_lname;
+    FETCH c_emp_cursor INTO l_empno, l_lname;
     EXIT WHEN c_emp_cursor%NOTFOUND;
-    DBMS_OUTPUT.PUT_LINE( v_empno ||'  '||v_lname);  
+    DBMS_OUTPUT.PUT_LINE( l_empno ||'  '||l_lname);  
   END LOOP;
 EXCEPTION
 WHEN NO_DATA_FOUND THEN
-DBMS_OUTPUT.PUT_LINE("The query did not return a result');
+DBMS_OUTPUT.PUT_LINE("The query with local did not return a result');
 END;
